3c1548c583a2bc0b49d38b3a71c7b704c7cfd5cc
takabo <thousandbeach.td@gmail.com>
2019-12-06 01:41:59 +0900

    vue-routerの導入とヒストリーモードの適用 web.phpからVue.jsのVueRouterへルーティングの変更 jQueryは使用しないので、bootstrap.jsのjQueryの記載をコメントアウト

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -7,11 +7,14 @@ window._ = require('lodash');
  * code may be modified to fit the specific needs of your application.
  */
 
+/*
+** jQuery は使用しないのでコメントアウト
 try {
     window.$ = window.jQuery = require('jquery');
 
     require('bootstrap-sass');
 } catch (e) {}
+*/
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
