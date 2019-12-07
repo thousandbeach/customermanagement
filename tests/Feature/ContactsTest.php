60b0468100cd6a58392b4705474235a6f422f21d
takabo <thousandbeach.td@gmail.com>
2019-12-07 16:22:55 +0900

    required のフィールドのテストのリファクタリング メソッドをまとめて、17のアサーションを3つのメソッドに集約した

--- tests/Feature/ContactsTest.php
+++ tests/Feature/ContactsTest.php
@@ -13,14 +13,7 @@ class ContactsTest extends TestCase
     /** @test */
     public function a_contact_can_be_added()
     {
-        $this->withExceptionHandling();
-
-        $this->post('/api/contacts', [
-            'name' => 'Test Name',
-            'email' => 'test@email.com',
-            'birthday' => '05/14/1988',
-            'company' => 'ABC String',
-        ]);
+        $this->post('/api/contacts', $this->data());
 
         $contact = Contact::first();
 
@@ -33,31 +26,26 @@ class ContactsTest extends TestCase
 
     }
 
-    /** @test */
-    public function a_name_is_required()
+    /** @test required のフィールドのテスト*/
+    public function fields_are_required()
     {
-        $response = $this->post('/api/contacts', [
+        collect(['name', 'email', 'birthday', 'company'])
+            ->each(function($field) {
+            $response = $this->post('/api/contacts', array_merge($this->data(), [$field => '']));
+
+        $response->assertSessionHasErrors($field);
+        // すべての連絡先がデータベースに追加されていないことをテストする。assertCount($count, $array) 配列$arrayの値の数が$countである。
+        $this->assertCount(0, Contact::all());
+        });
+    }
+
+    private function data()
+    {
+        return [
+            'name' => 'Test Name',
             'email' => 'test@email.com',
             'birthday' => '05/14/1988',
             'company' => 'ABC String',
-        ]);
-
-        $response->assertSessionHasErrors('name');
-        // すべての連絡先がデータベースに追加されていないことをテストする。assertCount($count, $array) 配列$arrayの値の数が$countである。
-        $this->assertCount(0, Contact::all());
-    }
-
-    /** @test */
-    public function email_is_required()
-    {
-        $response = $this->post('/api/contacts', [
-            'name' => 'Test Name',
-            'birthday' => '05/14/1988',
-            'company' => 'ABC String',
-        ]);
-
-        $response->assertSessionHasErrors('email');
-        // すべての連絡先がデータベースに追加されていないことをテストする。assertCount($count, $array) 配列$arrayの値の数が$countである。
-        $this->assertCount(0, Contact::all());
+        ];
     }
 }
