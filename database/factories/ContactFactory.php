6e302d4682559760d22719e1a86926daabcf4433
takabo <thousandbeach.td@gmail.com>
2019-12-17 01:18:19 +0900

    ContactsTest.php に only_the_users_contacts_can_be_retrieved()メソッドを追加して、テストをリファクタリングした。

--- database/factories/ContactFactory.php
+++ database/factories/ContactFactory.php
@@ -1,9 +1,11 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\User;
 
 $factory->define(App\Contact::class, function (Faker $faker) {
     return [
+        'user_id' => factory(User::class),
         'name' => $faker->name,
         'email' => $faker->email,
         'birthday' => '05/14/1988',
