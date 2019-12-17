f6379a2b3faade9a7e3edc83613eb9a7b44eeae0
takabo <thousandbeach.td@gmail.com>
2019-12-17 14:11:37 +0900

    ContactsTest.php ← 修正 ContactsController.php ← 修正 app/Http/Resources/Contact.php を php artisan make:resource Contact のコマンドで追加
    現在は、API に不要なものも含まれている。また、人が読むのに適した形式になっているとは言い難いので、修正する必要あり。return させて、jsonを直接viewに渡すのではなく、resource を作り、ここで整形しやすい形に変換してあげるようにする。

--- tests/Feature/ContactsTest.php
+++ tests/Feature/ContactsTest.php
@@ -35,7 +35,11 @@ class ContactsTest extends TestCase
 
         $response = $this->get('/api/contacts?api_token=' . $user->api_token);
 
-        $response->assertJsonCount(1)->assertJson([['id' => $contact->id]]);
+        $response->assertJsonCount(1)->assertJson([
+            'data' => [
+                ['contact_id' => $contact->id],
+            ]
+        ]);
     }
     /** @test
      *
@@ -113,10 +117,14 @@ class ContactsTest extends TestCase
         $contact = factory(Contact::class)->create(['user_id' => $this->user->id]);
         $response = $this->get('/api/contacts/' . $contact->id . '?api_token=' . $this->user->api_token);
         $response->assertJson([
-            'name' => $contact->name,
-            'email' => $contact->email,
-            'birthday' => $contact->birthday,
-            'company' => $contact->company,
+            'data' => [
+                'contact_id' => $contact->id,
+                'name' => $contact->name,
+                'email' => $contact->email,
+                'birthday' => $contact->birthday->format('m/d/Y'),
+                'company' => $contact->company,
+                'last_updated' => $contact->updated_at->diffForHumans(),
+            ]
         ]);
     }
 
